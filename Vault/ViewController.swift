914317b1cf79bcecc81855b8945d852361699ad3
Egor Shkarin <schkarin.egor@yandex.ru>
2026-02-01 16:16:10 +0400

    Initial Commit

---
+++ Vault/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vault
+//
+//  Created by Егор Шкарин on 01.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
